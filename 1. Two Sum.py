0ecb0709884f5942a89ba30e8c02d8afe2f4204a
Amogh Krishna <amoghkrishna55@gmail.com>
2023-08-01 19:09:26 +0530

    Added Solutions

---
+++ 1. Two Sum.py
@@ -0,0 +1,7 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        for i in range(0,len(nums)):
+            for j in range(0,len(nums)):
+                if nums[i]+nums[j] == target and i != j:
+                    return [i,j]
+                    break
