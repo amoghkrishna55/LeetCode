19954803d0723d36a3895848d93076312e057889
Amogh Krishna <amoghkrishna55@gmail.com>
2023-11-30 12:17:02 +0530

    Create file

---
+++ 26-RemoveDuplicatesfromSortedArray/26-RemoveDuplicatesfromSortedArray.cpp
@@ -0,0 +1,10 @@
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        flag = 1
+        curr = nums[0]
+        for i in range(1,len(nums)):
+            if(nums[i]!=curr):
+                nums[flag] = nums[i]
+                curr = nums[i]
+                flag += 1
+        return flag
